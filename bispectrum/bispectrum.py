29dee181f3f7c9c32fc21e82ef7519fecff080c5
Saroj Adhikari <adh.saroj@psu.edu>
2015-10-01 17:04:48 -0400

    add Kp vs Kb Kaiser factors and fix it, also add the integrated trispectrum fisher class

--- bispectrum/bispectrum.py
+++ bispectrum/bispectrum.py
@@ -25,11 +25,11 @@ class ibkLFisher(Fisher):
         kmin = 2.*np.pi/Lbox
         #kmax = self.survey.kmax
         b1 = self.survey.b1fid
+        Kp = self.Kaiser_factor_p(b1)
         #NkL = np.power(k/kmin, 2.0)  # check this
         NkL = np.power(Lbox, 3.0)*np.power(k, 2.0)*kmin/4./np.pi/np.pi
-        sigma=1.27*b1*np.sqrt(self.sigmaSqs[box])
-        #cpower = b1*b1*self.cosmology.power_spectrumz(k, self.survey.z)
-        cpower = 1.27*b1*b1*self.cosmology.power_spectrumz(k, self.survey.z)
+        sigma=b1*np.sqrt(Kp*self.sigmaSqs[box])
+        cpower = Kp*b1*b1*self.cosmology.power_spectrumz(k, self.survey.z)
         term1 = np.power(sigma, 2.0) + self.survey.Pshot/np.power(Lbox, 3.0)
         term2 = cpower + self.survey.Pshot
         
@@ -64,8 +64,15 @@ class ibkLFisher(Fisher):
         Kb = self.Kaiser_factor(b1)
         #Kb=1.0
         return Kb*(self.ibfNL(k, b1, fNL, box=box)+self.ibSPT(k, b1)+self.ibb2(k, b1, b2))
-        
-    def Kaiser_factor(self, b1):
+    
+    def Kaiser_factor_p(self, b1):
+        """return the Kaiser factor 1 + 2./3 beta + 1./5 beta^2 for the power spectrum
+        """
+        f = self.cosmology.growth_rate_f(self.survey.z)
+        beta = f/b1
+        return 1. + (2./3)*beta + (1./5)*np.power(beta, 2.0)
+    
+    def Kaiser_factor_b(self, b1):
         """return the Kaiser factor 1 + 2./3 beta + 1./9 beta^2
         where beta = f/b1
         """
@@ -125,6 +132,62 @@ class ibkLFisher(Fisher):
         integrand = lambda q, mu: q*q * np.power(top_hat(q, L), 2.0)* self.cosmology.power_spectrumz(np.sqrt(k*k+q*q-2*k*q*mu), self.survey.z)
         results = nquad(integrand, [[kmin, self.survey.kmax*5.], [-1.,1.]])
         return fac*results[0]
+
+class itkLFisher(ibkLFisher):
+    """ similar to ibkLFisher but for the squeezed trispectrum
+    """
+    
+    def DeltaitSq(self, k=0.5, box=0):
+        Lbox = self.survey.Lboxes[box]  # get the physical length of the box # specified
+        Vfactor = np.power(Lbox/self.survey.Lsurvey, 3.0)
+        kmin = 2.*np.pi/Lbox
+        #kmax = self.survey.kmax
+        b1 = self.survey.b1fid
+        #NkL = np.power(k/kmin, 2.0)  # check this
+        NkL = np.power(Lbox, 3.0)*np.power(k, 2.0)*kmin/4./np.pi/np.pi
+        sigma=1.27*b1*np.sqrt(self.sigmaSqs[box])
+        #cpower = b1*b1*self.cosmology.power_spectrumz(k, self.survey.z)
+        cpower = 1.27*b1*b1*self.cosmology.power_spectrumz(k, self.survey.z)
+        term1 = np.power(sigma, 2.0) + self.survey.Pshot/np.power(Lbox, 3.0)
+        term2 = cpower + self.survey.Pshot
+        
+        return Vfactor * term1 * np.power(term2, 3.0)/NkL/np.power(sigma, 4.0)/np.power(cpower, 4.0)
+        
+    def itk_deriv(self, k, param="fNL", box=0):
+        """ find the derivatives around the fiducial values defined in the survey class
+        since the fid fNL parameter can be 0, lets use param+0.01 for finite difference
+        of fNL, and fac=1.01 for the bias parameters that have non-zero fid values
+        """
+        fac=1.01
+        sv=self.survey
+        ibtfid=self.ibtotal(k, sv.b1fid, sv.b2fid, sv.fNLfid, box=box)
+        if param=="fNL":
+            ibkdif = self.ibtotal(k, sv.b1fid, sv.b2fid, sv.fNLfid+0.01, box=box)-ibtfid
+            return ibkdif/(0.01)
+        if param=="b1":
+            ibkdif = self.ibtotal(k, sv.b1fid*fac, sv.b2fid, sv.fNLfid, box=box)-ibtfid
+            return ibkdif/((fac-1.0)*self.survey.b1fid)
+        if param=="b2":
+            ibkdif = self.ibtotal(k, sv.b1fid, sv.b2fid*fac, sv.fNLfid, box=box)-ibtfid
+            return ibkdif/((fac-1.0)*self.survey.b2fid)
+            
+    def fisher(self, skip=1.):
+        """skip defines the binning scheme
+        """
+        fmatrix=np.array([[0.]*self.nparams]*self.nparams)
+        
+        for i in range(self.nparams):
+            for j in range(self.nparams):
+                total=0.0
+                for box in range(len(self.survey.Lboxes)):
+                    kmin = 2.*np.pi/self.survey.Lboxes[box]
+                    klist = np.arange(kmin, self.survey.kmax, skip*kmin)
+                    dibk_list = np.array([self.itk_deriv(k, param=self.param_names[i], box=box)*self.ibk_deriv(k, param=self.param_names[j], box=box)/self.DeltaitSq(k, box=box) for k in klist])
+                    total = total + np.sum(dibk_list)
+                fmatrix[i][j]=total
+
+        self.fisher_matrix=np.matrix(fmatrix)
+        return self.fisher_matrix
 
 class Survey:
     """ large-scale structure survey class with fixed z but kmin/kmax and V specified
