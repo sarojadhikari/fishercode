5ef715bf689199e1a5fa53840d3ebae5e73b4572
Saroj Adhikari <adh.saroj@psu.edu>
2016-02-23 15:37:20 -0500

    change return plt to return ax

--- fisher.py
+++ fisher.py
@@ -1,9 +1,9 @@
- 
+
 """
 .. module:: fisher
-   :synopsis: base class that defines the Fisher matrix class, manipulates it 
+   :synopsis: base class that defines the Fisher matrix class, manipulates it
     and performs simple computations
-    
+
 """
 
 import numpy as np
@@ -18,10 +18,10 @@ matplotlib.rcParams.update({'figure.autolayout': True})
 matplotlib.rcParams.update({'ytick.major.pad': 8})
 matplotlib.rcParams.update({'xtick.major.pad': 6})
 
-class Fisher(object): 
+class Fisher(object):
     """
-    This is the base fisher matrix class that is inherited by :class:`.ClusterFisher` 
-    and :class:`.CMBFisher` classes. 
+    This is the base fisher matrix class that is inherited by :class:`.ClusterFisher`
+    and :class:`.CMBFisher` classes.
     """
     def __init__(self, params=[], param_values=[], param_names=[], priors=[]):
         self.parameters=params
@@ -41,16 +41,16 @@ class Fisher(object):
             self.param_names=params
         else:
             self.param_names=param_names
-    
+
     def fisher(self):
         """
-        method to compute the fisher matrix given the parameters, survey and 
-        cosmology definitions for this class. The computations are performed in the inherited classes such as :class:`.CMBFisher` and :class:`.ClusterFisher`. 
+        method to compute the fisher matrix given the parameters, survey and
+        cosmology definitions for this class. The computations are performed in the inherited classes such as :class:`.CMBFisher` and :class:`.ClusterFisher`.
         """
         # loop over the parameters (and sum over redshift bins) to form the Fisher matrix
-        
+
         return np.array(self.fisher_matrix)  # numpy array for easy indexing
-    
+
     def add_priors(self, prior_inf=[]):
         """
         add prior information 1/sigma^2 to the fisher matrix; the prior matrix is given in
@@ -58,7 +58,7 @@ class Fisher(object):
         """
         if len(prior_inf)>0:
             self.priors=prior_inf
-        # else use existing prior information    
+        # else use existing prior information
         prior_matrix=np.array([[0.]*self.nparams]*self.nparams)
         for i in range(self.nparams):
             if self.priors[i]>0:
@@ -66,22 +66,22 @@ class Fisher(object):
         self.fisher_matrix=self.fisher_matrix+np.matrix(prior_matrix)
         self.prior_information_added=True
         return self.fisher_matrix
-        
+
     def covariance(self):
         """
         compute the inverse of fisher matrix
         """
         self.covariance_matrix=self.fisher_matrix.I
         return np.array(self.covariance_matrix)
-    
+
     def marginalize(self, param_list=[0,1]):
         """
-        compute and return a new fisher matrix after marginalizing over all 
+        compute and return a new fisher matrix after marginalizing over all
         other parameters not in the list param_list
-        
+
         also save the marginalized fisher matrix in self.marginal_fisher_matrix
         and save the new covariance matrix in self.marginal_covariance_matrix
-        """ 
+        """
         npars=self.nparams
         trun_cov_matrix=self.covariance_matrix
         mlist=[]
@@ -90,12 +90,12 @@ class Fisher(object):
                 mlist.append(i)
         trun_cov_matrix=np.delete(trun_cov_matrix, mlist, 0)
         trun_cov_matrix=np.delete(trun_cov_matrix, mlist, 1)
-        
+
         self.marginal_covariance_matrix=trun_cov_matrix
         self.marginal_fisher_matrix=trun_cov_matrix.I
         self.martinal_param_list=param_list
         return trun_cov_matrix.I
-    
+
     def save_fisher_matrix(self, fname):
         """
         save the current fisher matrix and relevant information in the DETF format as name.fisher
@@ -103,47 +103,48 @@ class Fisher(object):
         hd=""
         for param in self.parameters:
             hd=hd+param+" "
-            
+
         np.savetxt(fname, self.fisher_matrix, header=hd)
         return 0
-    
+
     def load_fisher_matrix(self, fname):
         """
         load the fisher matrix in file fname. currently, one has to set the parameter names etc
         manually.
         """
         self.fisher_matrix=np.matrix(np.loadtxt(fname))
+        self.covariance()
         return 0
-        
+
     def sigma_i(self, i):
         """
         return the 1-sigma error for the ith parameter (given a 2x2 cov matrix)
         """
         return np.sqrt(np.array(self.covariance_matrix)[i][i])
-    
+
     def corr_coeff(self, i, j):
         """
         return the correlation coefficient between ith and jth parameters
         """
         sigma_ij=self.covariance_matrix.item(i,j)
         return sigma_ij/self.sigma_i(i)/self.sigma_i(j)
-     
+
     def error_ellipse(self, i, j, nstd=1, space_factor=5., clr='b', alpha=0.5, lw=1.5):
         """
         return the plot of the error ellipse from the covariance matrix
-        use ideas from error_ellipse.m from 
+        use ideas from error_ellipse.m from
         http://www.mathworks.com/matlabcentral/fileexchange/4705-error-ellipse
         ( the version used here was taken from an earlier version, it looks to have been updated there to a new version.)
-        
+
         * (i,j) specify the ith and jth parameters to be used and
-        * nstd specifies the number of standard deviations to plot, the default is 1 sigma. 
+        * nstd specifies the number of standard deviations to plot, the default is 1 sigma.
         * space_factor specifies the number that divides the width/height, the result of which is then added as extra space to the figure
         """
         def eigsorted(cov):
             vals, vecs = np.linalg.eigh(cov)
             order = vals.argsort()[::1]
             return vals[order], vecs[:,order]
-        
+
         self.marginalize(param_list=[i,j])
         vals, vecs = eigsorted(self.marginal_covariance_matrix)
         theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
@@ -160,7 +161,7 @@ class Fisher(object):
         dy=(max(yl)-min(yl))/space_factor
         xyaxes=[min(xl)-dx, max(xl)+dx, min(yl)-dy, max(yl)+dy]
         return ellip, xyaxes
-            
+
     def plot_error_ellipse(self, i, j, xyaxes_input=0, nstd=1, clr='b', alpha=0.5, lw=1.5):
         """
         """
@@ -171,12 +172,12 @@ class Fisher(object):
             ax.axis(xyaxes_input)
         else:
             ax.axis(xyaxes)
-            
+
         plt.xlabel(self.param_names[i], fontsize=16)
         plt.ylabel(self.param_names[j], fontsize=16)
         #plt.show()
-        return plt
-        
+        return ax
+
     def plot_error_matrix(self, params, nstd=1, nbinsx=6, nbinsy=6):
         """ plot a matrix of fisher forecast error ellipses given the
         list of parameters provided
@@ -185,13 +186,13 @@ class Fisher(object):
         #plt.figure(num=None, figsize=(fac*xs, fac*ys))
         plt.close('all')
         plt.ticklabel_format(style='sci', axis='both', scilimits=(-3,3))
-        
+
         f, allaxes = plt.subplots(fac, fac, sharex="col", sharey="row")
         for i in range(fac):
             for j in range(fac):
                 if (j>i):
                     allaxes[i][j].axis('off')
-                    
+
         if fac<2:
             errorellipse, xyaxes=self.error_ellipse(params[0],params[1], nstd=nstd, clr="b", alpha=0.4)
             ere2, xyaxes2 = self.error_ellipse(params[0], params[1], nstd=2, clr="r", alpha=0.1)
@@ -199,7 +200,7 @@ class Fisher(object):
             allaxes.add_artist(errorellipse)
             allaxes.axis(xyaxes)
             allaxes.set_xlabel(self.param_names[0], fontsize=14)
-            allaxes.set_ylabel(self.param_names[1], fontsize=14) 
+            allaxes.set_ylabel(self.param_names[1], fontsize=14)
         else:
             for j in params:
                 for i in params:
@@ -218,4 +219,3 @@ class Fisher(object):
                         axis.set_xlabel(self.param_names[i], fontsize=14)
                         axis.set_ylabel(self.param_names[j], fontsize=14)
                         #allaxes[jp][ip].set_title(str(jp)+","+str(ip))
-                        
